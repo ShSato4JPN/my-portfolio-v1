14b1252917ca9cfce3d0ba71709f8ec0e25ab3f6
ShSato4JPN <shsato4.private.1221@gmail.com>
2021-06-21 22:38:19 +0900

    とりあえずコミット。Hooksがイマイチ理解できていないため、勉強する。。

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
+import layout from '../styles/Home.module.css'
+
+const handleKeyDown = event => {
+  console.log( event.target.keyCode )
+}
+
 export default function Home() {
+  let history = []
   return (
-    <div>
+    <div className={layout.container}>
+      <div className={layout.console}>
+      </div>
+      <div className={layout.input_form}>
+        <input 
+          type="text"
+          className={layout.input_in}
+          onChange={handleKeyDown}
+          placeholder="Enter Command"
+        />
+      </div >
     </div>
   )
 }
