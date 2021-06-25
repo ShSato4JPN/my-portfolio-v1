abb67305ecf0de37e2bbf90425001e363796a7c0
ShSato4JPN <shsato4.private.1221@gmail.com>
2021-06-25 22:51:26 +0900

    エラーが残っているが一旦コミット（個人開発のため。。）

--- pages/components/Main/Main.js
+++ pages/components/Main/Main.js
@@ -1,30 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Profile from '../Browser/Browser'
 import styles from './Main.module.css'
 
 function Main () {
+  
   const [modeProfile, setModeProfile] = useState(false)
   const [modeBlog, setModeBlog] = useState(false)
   const [modeInformation, setModeInformation] = useState(false)
+  const [cmdHistory, setCmdHistory] = useState([])
 
-  useEffect( () => {
-    if (modeProfile) {
-      setModeBlog(false)
-    }
-  })
+  // Input Command Logs
+  let cmdLog = cmdHistory
 
   // State Functions Wrappers
-  const closeProfile = () => {
-    setModeProfile(false)
-  }
-
-  const closeBlog = () => {
-    setModeBlog(false)
-  }
-
-  const closeInformation = () => {
-    setModeInformation(false)
-  }
+  const closeProfile = () => { setModeProfile(false) }
+  const closeBlog = () => { setModeBlog(false) }
+  const closeInformation = () => { setModeInformation(false) }
 
   // Key Event Handlers
   const handleKeyDown = ( event ) => {
@@ -36,18 +27,22 @@ function Main () {
           setModeProfile(true)
           setModeBlog(false)
           setModeInformation(false)
+          setCmdHistory('show profile is running...')
           break;
         case 'show blog':
           setModeProfile(false)
           setModeBlog(true)
           setModeInformation(false)
+          setCmdHistory('show blog is running...')
           break;
-        case 'show information':
+        case 'show info':
           setModeProfile(false)
           setModeBlog(false)
           setModeInformation(true)
+          setCmdHistory('show info is running...')
           break;
         default:
+          setCmdHistory('No such command exists...')
           break;
       }
     }
@@ -56,11 +51,14 @@ function Main () {
   return (
     <div>
       <div className={styles.consoleform}>
-        <div className={ styles.browsers } >
+        {cmdLog.map ( (item, index) => (
+          <p index={index}>{item}</p>
+        ))}
+      </div>
+      <div className={ styles.browsers } >
           {modeProfile ? <Profile title={ 'PROFILE' } closeAction={ closeProfile } /> : ''}
           {modeBlog ? <Profile title={ 'BLOG' } closeAction={ closeBlog } /> : ''}
           {modeInformation ? <Profile title={ 'INFORMATION' } closeAction={ closeInformation } /> : ''}
-        </div>
       </div>
       <div className={styles.inputform}>
         <input type="text" onKeyDown={handleKeyDown} />
