8e973ab3887cbe259f3c94d6fc124bb54c6e3efe
ShSato4JPN <shsato4.private.1221@gmail.com>
2021-07-02 22:52:14 +0900

    Profile画面の開発途中

--- pages/components/Profile/Profile.js
+++ pages/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import Browser from '../Browser/Browser'
 import styles from './Profile.module.css'
 
+import Image from 'next/image'
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
@@ -27,7 +28,7 @@ function TabPanel(props) {
   );
 }
 
-export default function Profile ( props ) {
+export default function Profile () {
   const title = 'Profile'
   const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
@@ -43,7 +44,7 @@ export default function Profile ( props ) {
             value={value}
             onChange={handleChange}
             indicatorColor="primary"
-            textColor="blue"
+            textColor="primary"
             centered
           >
             <Tab label={<span className={styles.tab}>自己紹介</span>} />
@@ -52,7 +53,21 @@ export default function Profile ( props ) {
           </Tabs>
         </Paper>
         <TabPanel value={value} index={0}>
-          <h1>hello</h1>
+          <div>
+            <div className={styles.profile_head}>
+              <div className={styles.profile_head_left}>
+                <Image src={'/../public/image/profile_image.png'}
+                       height={200}
+                       width={200}
+                       alt='profile image'
+                />
+              </div>
+              <div className={styles.profile_head_right}>
+              </div>
+            </div>
+            <div className={styles.profile_body}>
+            </div>
+          </div>
         </TabPanel>
         <TabPanel value={value} index={1}>
           Item Two
